61ba8ed6f043930aefeadad318ec51acaff1eb45
Ergin TIRAVOGLU <ergintiravoglu@msn.com>
2023-11-07 23:40:53 +0300

    Angular login auth guard and generic http service added.

--- AngularApp/src/main.ts
+++ AngularApp/src/main.ts
@@ -1,16 +1,19 @@
-import { BrowserModule, bootstrapApplication } from "@angular/platform-browser";
-import { AppComponent } from "./app/app.component";
 import { importProvidersFrom } from "@angular/core";
+import { BrowserModule, bootstrapApplication } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
-
+import { AppComponent } from "./app/app.component";
+import { AuthGuard } from "./app/ui/components/auth/guards/auth.guard";
+import {provideHttpClient} from "@angular/common/http";
 bootstrapApplication(AppComponent,{
   providers:[
+    provideHttpClient(),
     importProvidersFrom(
       BrowserModule,
       RouterModule.forRoot([
         {
           path:"",
           loadComponent:()=>import("./app/ui/components/layouts/layouts.component").then(c=>c.LayoutsComponent),
+          canActivateChild:[AuthGuard],
           children: [
             {
               path:"",
@@ -18,10 +21,10 @@ bootstrapApplication(AppComponent,{
             }
           ]
         },
-{
-  path:"login",
-  loadComponent:()=> import("./app/ui/components/auth/login/login.component").then(c=>c.LoginComponent)
-}
+        {
+          path:"login",
+          loadComponent:()=> import("./app/ui/components/auth/login/login.component").then(c=>c.LoginComponent)
+        }
       ])
     )
   ]
